b68abae3db6b95109bf6fdbc62667c1274b8f650
Deepak Kapoor <nk93555@gmail.com>
2022-12-03 23:42:18 +0530

    Updated css and new URL's

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 export const products = [
   {
     id: 1,
-    name: "Chatify",
-    link: "https://chatify-deepak-kapoor.herokuapp.com/",
-    desc: "Chatting app based on MongoDB, Express.js, Node.js, React.js"
+    name: "Chatify - Deepak Kapoor",
+    link: "https://chatify-deepak-kapoor.vercel.app/",
+    desc: "Chatting app developed using MongoDB, Express.js, Node.js, React.js"
   },
   {
     id: 2,
-    name: "NewsFeed",
-    link: "https://newsfeed-deepak-kapoor.herokuapp.com/",
+    name: "NewsFeed - Deepak Kapoor",
+    link: "https://news-feed-deepak-kapoor.vercel.app/",
     desc: "News app used to fetch the latest news based on categories."
   },
   {
     id: 3,
-    name: "MyPlantsTalk",
+    name: "MyPlantsTalk - Deepak Kapoor",
     link: "https://deepak5151.github.io/MyPlantsTalk/",
     desc: "Informative app where you can find out about different plants and their nature."
   },
